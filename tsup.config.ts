7e6ab92e15ad7541388d7c42707d9fb8a668ec83
zkh <1650697374@qq.com>
2026-06-01 13:58:30 +0800

    优化打包大小

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,9 +5,19 @@ export default defineConfig({
   format: ['cjs', 'esm'],
   dts: true,
   splitting: false,
-  sourcemap: true,
+  sourcemap: false, // 关闭 sourcemap，减小包体积约 370KB
   clean: true,
   outDir: 'dist',
-  minify: false,
+  minify: true, // 启用代码压缩，进一步减小体积
+  terserOptions: {
+    compress: {
+      drop_console: true, // 移除 console.log
+      drop_debugger: true, // 移除 debugger
+      pure_funcs: ['console.log', 'console.info', 'console.debug'], // 移除这些函数调用
+    },
+    format: {
+      comments: false, // 移除注释
+    },
+  },
   external: [],
 });
